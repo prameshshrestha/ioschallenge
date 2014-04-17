6918a115260990aa115e8e8f3286d91af46fdac9
pramesh <pramesh@192.168.113.129>
2014-04-17 16:17:16 -0500

    added json

--- PocketScience/ViewController.m
+++ PocketScience/ViewController.m
@@ -24,6 +24,36 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSURL *restUrl = [NSURL URLWithString:@"http://www.nousguideinc.com/12323123dsfsdf/4358h.json"];
+    NSError *error = nil;
+    NSData *data = [NSData dataWithContentsOfURL:restUrl options:NSDataReadingUncached error:&error];
+    if(!error)
+    {
+        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&error];
+        NSMutableArray *array = [json objectForKey:@"GetAllTrackingPointsResult"];
+        
+        dict = [[NSMutableDictionary alloc]init];
+        for (int i = 0; i < array.count; i++)
+        {
+            trackInfo = [array objectAtIndex:i];
+            NSString *ttype = [trackInfo objectForKey:@"TType"];
+            NSString *tname = [trackInfo objectForKey:@"TName"];
+            [dict setValue:tname forKey:ttype];
+        }
+        name = [[NSMutableArray alloc]init];
+        for (id key in dict)
+        {
+            id value = [dict objectForKey:key];
+            [name addObject:value];
+        }
+    }
+    else
+    {
+        UIAlertView *err = [[UIAlertView alloc]initWithTitle:@"Server Error" message:@"Cannot connect to the server" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [err show];
+    }
 }
 
 - (void)didReceiveMemoryWarning
